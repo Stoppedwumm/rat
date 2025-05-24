7f22254741c47fae4ddbccd833914acc5ea342d8
stoppedwumm <mobileboxnews@icloud.com>
2025-05-24 15:42:28 +0200

    Aktualisiere die Konfigurationsdatei für den Agenten, um den "prebuilt"-Wert korrekt zu setzen und verbessere das Build-Skript für die Ausgabe der Konfiguration.

--- build.sh
+++ build.sh
@@ -45,14 +45,10 @@ echo Building Agent
 cd AgentClient
 if [ $# -eq 1 ] && [ $1 == "--prebuilt" ]; then
     echo "Using prebuilt agent"
-    echo '{
-    "prebuilt": true
-}' > app/src/main/resources/config.json
+    echo '{ "prebuilt": true }' > app/src/main/resources/config.json
 else
     echo "Using custom agent"
-    echo '{
-    "prebuilt": false
-}' > app/src/main/resources/config.json
+    echo '{ "prebuilt": false }' > app/src/main/resources/config.json
 fi
 
 ./gradlew clean
